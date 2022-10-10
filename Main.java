7e31d50b18b80947a766d0908a8a944af58c1d36
Mattia Masper <101709283+maspermattia@users.noreply.github.com>
2022-10-10 10:48:30 +0000

    lavoro finito

--- Main.java
+++ Main.java
@@ -1,18 +1,18 @@
 import java.util.Random;
 class Ruota extends Thread{
   private String c;
-  private int ritardo;
+  private int ri;
 
-  public Ruota(String c, int ritardo){
+  public Ruota(String c, int ri){
     this.c=c;
-    this.ritardo=ritardo;
+    this.ri=ri;
   }
   public void run(){
     for(int i=0;i<5;i++){
       Random r=new Random();
   System.out.println(c+""+(r.nextInt(90)+1));
       try{
-        sleep(ritardo);
+        sleep(ri);
       }
      catch(Exception e){
         System.out.println("errore");
